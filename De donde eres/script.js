ffd1e027498459fa6d504d939e7a560a797c191c
HelixAs <arizdaniel09@gmail.com>
2025-10-07 15:55:27 -0500

    Create script.js

---
+++ De donde eres/script.js
@@ -0,0 +1,15 @@
+var argentino = prompt("eres argentino? si o no?");
+
+if (argentino ==="si") {
+    alert ("tu eres Argentino");
+ }    else { 
+    alert ("no eres argentino");
+} 
+
+var venezolano = prompt ("eres de venezuela? si o no?");
+
+if (venezolano === "si") {
+    alert("tu eres venezolano");
+}       else {
+    alert ("no se de donde eres!")
+}   
