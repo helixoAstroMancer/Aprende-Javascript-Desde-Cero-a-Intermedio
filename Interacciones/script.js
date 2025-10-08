39e67294aa0747c5657ad76e87b205654e7548fc
HelixAs <arizdaniel09@gmail.com>
2025-10-07 19:32:00 -0500

    Create script.js

---
+++ Interacciones/script.js
@@ -0,0 +1,19 @@
+var list = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"];
+/*
+for ( var i =0; i < list.length; i++) {
+    console.log(list [i] + " hola ");
+}
+*//*
+var conter = 0;
+while (conter < list.length) {
+    console.log(conter);
+    conter++;
+}
+*/
+/*
+var conter = 10;
+do {
+    console.log(conter);
+    conter--;
+} while (conter > 0); 
+*/
